b3389a5b2eadadbf65b8a9b7b06ec52b8e2fca9a
Adriana Aguilar <agruad@gmail.com>
2022-12-23 17:30:44 +0100

    deleted comments

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 const requestURL = './json/canciones.json';
-//Llamada asíncrona para que lea todos los datos antes de continuar
+
 async function fetchSongsJson(){
     const response = await fetch(requestURL);
-    const songs = await response.json(); //canciones.json estará dentro de la variable "songs"
+    const songs = await response.json();
     return songs;
 }
-//Promesa para que lea el archivo json
+
 fetchSongsJson().then(songs =>{
     for (let index = 0; index < songs.canciones.length; index++){
     
@@ -38,29 +38,24 @@ fetchSongsJson().then(songs =>{
 
 const buttonNewSong = document.querySelector('#buttonNewSong');
 const formAddSong = document.querySelector('#formAddSong');
-//Función para extraer todos los datos del formulario y convertirlos en formato JSON
+
 const getData = () => {
-    //Constructor que crea un objeto de tipo FormData
     const data = new FormData(formAddSong);
-    //Método Object.fromEntries() transforma una lista de pares con [clave-valor] en un objeto
     const processedData = Object.fromEntries(data.entries());
     formAddSong.reset();
     return processedData;
 }
-//Función para colocar los datos en el servidor
 const postData = async () => {
-    //Crea un objeto con la información del formulario
     const newSong= getData();
     try {
         const response = await fetch ('http://localhost:3000/canciones', {
-            method: 'POST', //especifica el método que se va a usar
-            headers:{'Content-Type': 'application/json'}, //especifica el tipo de contenido
-            body:JSON.stringify(newSong) //coloca la información en formato JSON
+            method: 'POST',
+            headers:{'Content-Type': 'application/json'},
+            body:JSON.stringify(newSong)
         });
 
         if(response.ok){
             const jsonResponse = await response.json();
-            //Código que usa la respuesta
             const {tema, artista, genero, enlace} = jsonResponse;
         }
     } catch (error) {
